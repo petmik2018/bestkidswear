103e67ffda3358961b5b86bc8cb4642481fff937
mikhail <petmik@yandex.ru>
2022-02-19 13:45:52 +0200

    settings back to prod

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,8 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = "http://127.0.0.1:8000"
+//axios.defaults.baseURL = "http://127.0.0.1:8000"
 //axios.defaults.baseURL = "http://127.0.0.1"
-// axios.defaults.baseURL = 'http://80.78.246.218'
+axios.defaults.baseURL = 'http://80.78.246.218'
 
 createApp(App).use(store).use(router, axios).mount('#app')
